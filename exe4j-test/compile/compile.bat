dd12d2abee4403d1524347d2e5f9457261ecb894
Kosaka Bun <kosaka-bun@qq.com>
2022-10-08 22:43:31 +0800

    完成对exe4j的测试

--- exe4j-test/compile/compile.bat
+++ exe4j-test/compile/compile.bat
@@ -2,6 +2,7 @@ set PROJECT_NAME="exe4j-test"
 
 cd ..
 del /s /q .\build\libs\*
+for /r .\build\libs /d %%a in (*) do rmdir /s /q "%%a"
 call gradlew bootJar
 cd compile
 
--- exe4j-test/src/main/java/de/honoka/test/exe4j/Exe4jTest.java
+++ exe4j-test/src/main/java/de/honoka/test/exe4j/Exe4jTest.java
@@ -1,5 +1,6 @@
 package de.honoka.test.exe4j;
 
+import de.honoka.sdk.util.system.gui.ConsoleWindow;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Exe4jTest {
 
     public static void main(String[] args) {
+        ConsoleWindow window = new ConsoleWindow("Test",
+                null, () -> {});
+        window.setAutoScroll(true);
+        window.setScreenZoomScale(1.25);
+        window.show();
         SpringApplication.run(Exe4jTest.class, args);
     }
 }

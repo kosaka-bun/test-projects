4af942736b4533b3a1ad8606d08961ad5675c4ae
Kosaka Bun <kosaka-bun@qq.com>
2023-07-28 20:39:13 +0800

    Update circular-reference-test

---
+++ old/circular-reference-test/test1/src/main/java/de/honoka/test/spring/crt/test1/Test1.kt
@@ -0,0 +1,49 @@
+package de.honoka.test.spring.crt.test1
+
+import org.springframework.boot.ApplicationArguments
+import org.springframework.boot.ApplicationRunner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Lazy
+import org.springframework.stereotype.Component
+import javax.annotation.Resource
+
+@SpringBootApplication
+class CrtTest1Application
+
+fun main(args: Array<String>) {
+    runApplication<CrtTest1Application>(*args)
+}
+
+@Component
+class Bean1 : ApplicationRunner {
+
+    @Resource
+    private var bean2: Bean2? = null
+
+    override fun run(args: ApplicationArguments) {
+        println("Bean1")
+        println(bean2)
+    }
+
+    override fun toString(): String {
+        return "Bean1: bean2=" + (bean2?.hashCode() ?: "null")
+    }
+}
+
+@Component
+class Bean2 : ApplicationRunner {
+
+    @Lazy
+    @Resource
+    private var bean1: Bean1? = null
+
+    override fun run(args: ApplicationArguments) {
+        println("Bean2")
+        println(bean1)
+    }
+
+    override fun toString(): String {
+        return "Bean2: bean1=" + (bean1?.hashCode() ?: "null")
+    }
+}

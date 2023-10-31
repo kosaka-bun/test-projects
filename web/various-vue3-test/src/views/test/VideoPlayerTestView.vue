83ff8bed20015217b2bde35e7ff5aa16093d0998
Kosaka Bun <kosaka-bun@qq.com>
2023-10-31 22:27:43 +0800

    various-vue3-test/VideoPlayerTestView使播放器容器适应高度大于min-height的视频

--- web/various-vue3-test/src/views/test/VideoPlayerTestView.vue
+++ web/various-vue3-test/src/views/test/VideoPlayerTestView.vue
@@ -1,13 +1,21 @@
 <template>
   <div>
     <h1>Video Player Test</h1>
+    <p>
+      <span>横屏：BV14N4y1N7H9</span>
+      <span style="margin-left: 2em;">竖屏：BV15W4y1n7VK</span>
+    </p>
+    <p>
+      BVID：<el-input v-model="bvid" style="width: 15em;" />
+      <el-button style="margin-left: 1em;" @click="playVideo">播放</el-button>
+    </p>
     <div class="player" ref="playerDom"></div>
   </div>
 </template>
 
 <script setup>
 import { onMounted, ref } from 'vue'
-import Player from 'nplayer'
+import Player, { EVENT } from 'nplayer'
 import Danmaku from '@nplayer/danmaku'
 import request from '@/utils/request'
 
@@ -15,15 +23,21 @@ const playerDom = ref()
 
 let player
 
+let videoDom
+
+const bvid = ref('BV14N4y1N7H9')
+
+//该页面组件在挂载完成（显示在页面上）时，注册窗口大小变化事件监听器
+//resize监听器会在浏览器刷新页面前持续存在，故当RouterView中的该组件即将被切换为其他组件时，必须移除这一监听器
 onMounted(() => {
   initPlayer()
-  loadDanmakuList()
+  playVideo()
 })
 
 function initPlayer() {
-  //noinspection JSCheckFunctionSignatures
+  videoDom = document.createElement('video')
   player = new Player({
-    src: 'http://pc.honoka.de:8080/bilibili/video/stream?bvid=BV14N4y1N7H9',
+    video: videoDom,
     controls: [
       [
         'play',
@@ -37,15 +51,26 @@ function initPlayer() {
       ],
       [ 'progress' ]
     ],
+    volumeVertical: true,
     plugins: [
       new Danmaku({
         autoInsert: false,
         unlimited: true,
-        area: 1
+        area: 1,
+        fontsizeScale: 0.85
       })
     ]
   })
+  player.on(EVENT.LOADED_METADATA, onVideoMetaDataLoaded)
   player.mount(playerDom.value)
+  playerDom.value.style.backgroundColor = 'white'
+}
+
+function playVideo() {
+  let url = `http://pc.honoka.de:8080/bilibili/video/stream?bvid=${bvid.value}`
+  if(videoDom.src === url) return
+  videoDom.src = url
+  loadDanmakuList()
 }
 
 function loadDanmakuList() {
@@ -53,7 +78,7 @@ function loadDanmakuList() {
     url: 'http://pc.honoka.de:8080/bilibili/video/danmaku',
     method: 'get',
     params: {
-      bvid: 'BV14N4y1N7H9'
+      bvid: bvid.value
     }
   }).then(res => {
     let list = [ ...res.data ]
@@ -62,17 +87,42 @@ function loadDanmakuList() {
       //noinspection JSUnresolvedReference
       it.color = it.colorRgb
     })
-    player.danmaku.appendItems(list)
+    player.danmaku.resetItems(list)
   })
+}
+
+/**
+ * 视频的元数据加载完成时触发，元数据包括：时长、尺寸以及文本轨道
+ */
+function onVideoMetaDataLoaded() {
+  adjustPlayerHeight()
+}
+
+/**
+ * 若视频高度大于容器最小高度，则将容器高度调整为视频高度，
+ * 若视频高度小于容器最小高度，则使播放器高度充满容器最小高度
+ */
+function adjustPlayerHeight() {
+  //设置了height后，若视频高度大于容器的height或min-height，则播放器高度将不会自适应为视频的高度，需事先移除height
+  playerDom.value.style.removeProperty('height')
+  let style = window.getComputedStyle(playerDom.value)
+  let minHeight = Number(style.minHeight.match(/\d+/g)[0])
+  let height = Number(style.height.match(/\d+/g)[0])
+  if(height <= minHeight) {
+    /*
+     * 若不明确设置height，会导致在播放器挂载到容器之后，容器的min-height不会对播放器生效
+     * （播放器高度不会充满容器的min-height）
+     */
+    playerDom.value.style.height = '0'
+  }
 }
 </script>
 
 <style scoped lang="scss">
 .player {
   width: 800px;
-  //若不明确设置height，会导致在播放器挂载到容器之后，容器的min-height不会对播放器生效（播放器高度不会充满容器的min-height）
-  height: 0;
-  min-height: calc((9 / 16.0) * 800px);
+  min-height: 450px;
+  background-color: black;
 }
 
 @media(max-width: 800px) {

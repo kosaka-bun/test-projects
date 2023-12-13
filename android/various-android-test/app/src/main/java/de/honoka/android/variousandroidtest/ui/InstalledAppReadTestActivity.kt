84773b147bf4bc82b817a1080d1f4a782ab36c62
Kosaka Bun <kosaka-bun@qq.com>
2023-12-13 17:14:46 +0800

    Add various-android-test

---
+++ android/various-android-test/app/src/main/java/de/honoka/android/variousandroidtest/ui/InstalledAppReadTestActivity.kt
@@ -0,0 +1,13 @@
+package de.honoka.android.variousandroidtest.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import de.honoka.android.variousandroidtest.R
+
+class InstalledAppReadTestActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_installed_app_read_test)
+    }
+}
